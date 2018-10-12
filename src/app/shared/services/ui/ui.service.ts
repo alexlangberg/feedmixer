139cfea4c90b8d803fa5db9fbe335e4ac56a534c
Alex Langberg <mail@alexlangberg.dk>
2018-10-12 15:02:56 +0200

    Add closing buttons for menus.

--- src/app/shared/services/ui/ui.service.ts
+++ src/app/shared/services/ui/ui.service.ts
@@ -78,10 +78,12 @@ export class UIService implements OnDestroy {
 
   doToggleSidenav() {
     this.sidenav.toggle();
+    this.isSidenavOpen = this.sidenav.opened;
   }
 
   doToggleSidenavEnd() {
     this.sidenavEnd.toggle();
+    this.isSidenavEndOpen = this.sidenavEnd.opened;
   }
 
   onShowItemInfo(url: string) {
